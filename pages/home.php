fcbe31957f35c46da96221c5fe2f341f99694ca6
kudrina1506 <kudrina1506@gmail.com>
2022-11-22 23:10:00 +0300

    lesson 3

--- pages/home.php
+++ pages/home.php
@@ -1,2 +1,78 @@
-<h1>HOME</h1>
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../normalize.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<style>
+    html, body{
+        width: 100%;
+        height: 100%;
+    }
+    #app {
+        background-color: grey;
+        width: 100%;
+        height: 100%;
+    }
+    .header {
+        background-color: pink;
+        width: 100%;
+        height: 10%;
+    }
+    .content {
+        background-color: green;
+        width: 100%;
+        height: 80%;
+    }
+    .footer {
+        background-color: violet;
+        width: 100%;
+        height: 10%;
+    }
+    .columns {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+    #col1 {
+        background-color: yellow;
+        width: 200px;
+    }
+    #col2 {
+        background-color: red;
+        flex-grow: 1;
+    }
+    #col3 {
+        background-color: blue;
+        width: 100px;
+    }
+    
+</style>
+ 
+<div id="app">
+    <div class="header">
+
+    </div>
+    <div class="content">
+         <div class="columns">
+            <div id="col1">col1</div>
+            <div id="col2">col2</div>
+            <div id="col3">col3</div>
+         </div>
+    </div>
+    <div class="footer">
+
+    </div>
+</div>
+
+</body>
+</html>
+
+
+
--- router.php
+++ router.php
@@ -6,9 +6,16 @@ function router(string $url, Closure $callback)
 {
     global $router;
     $route = trim($url, "\t\n\r\0\x0B");
-    $route = $url === '/'
-        ? '/'
-        : preg_replace('/\/{0,}$/', '', $route);
+    //$route = $url === '/'
+    //    ? '/'
+    //    : preg_replace('/\/{0,}$/', '', $route);
+
+    if ($url === '/') {
+        $route = '/';
+    }
+    else {
+        $route = preg_replace('/\/{0,}$/', '', $route);
+    }
     $router[$route] = $callback;
 }
 
