15f5cfd91b7a0f4229af506efec95fea48b26167
kudrina1506 <kudrina1506@gmail.com>
2022-12-02 20:07:46 +0300

    make_starships_page

---
+++ pages/starships.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'pages/layout/header_html.php';
+
+require_once 'pages/header/header.php';
+
+require_once 'pages/contents/content_home.php';
+
+require_once 'pages/layout/footer_html.php';
+?>
